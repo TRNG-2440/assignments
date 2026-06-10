549e6d14defdf2e2a0dc08e98fa2e7544e032fc0
Kyle Drewes <kyle_drewes@yahoo.com>
2026-06-10 16:09:38 -0700

    Constructed all functions

--- assignment_004/WordFrequency.py
+++ assignment_004/WordFrequency.py
@@ -19,3 +19,49 @@ def Menu():
   '\n4. Clear results'
   '\n0. Exit\n')
 
+# Algorithm used to analyze sentence
+def AnalyzeSentence() -> list:
+  sentence = input('Enter sentence: ')
+
+  return [w.strip() for w in sentence.split()]
+
+# Determine the number of words within a sentence
+def WordCount(wordList) -> None:
+
+  # Declare dictionary to keep track of each word
+  wordDictionary = dict()
+
+  # Determine
+  for w in wordList:
+    wordDictionary[w] += 1
+
+  # Determine word count for each word
+  for w in wordList:
+    print(f'{w} : {wordDictionary[w]}')
+  
+# Determine most frequent word
+def MostFrequentWord(wordList) -> int:
+
+# Declare dictionary to keep track of each word
+  wordDictionary = dict()
+
+  # Determine word count for each word
+  for w in wordList:
+    wordDictionary[w] += 1
+
+  # Return most frequent word
+  return list.max(wordDictionary.values())
+
+# Clear word
+def ClearWord(wordList) -> None:
+  wordList.clear()
+
+# Main menthod
+def main():
+
+  # Display main menu
+  Menu()
+
+
+
+main()
