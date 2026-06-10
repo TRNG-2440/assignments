fa1664bafde1a28c9244c933451b26b15ad8356d
Kyle Drewes <kyle_drewes@yahoo.com>
2026-06-10 11:57:07 -0700

    Completed menu

---
+++ assignment_003/ToDoList.py
@@ -0,0 +1,22 @@
+"""Build a console-based To-Do List Manager that 
+allows a user to add tasks, view all tasks, mark 
+tasks as complete, and remove completed tasks. 
+This activity focuses on lists and practices 
+while loops,if-elif-else, for loops, input(), 
+and list methods including append, remove, and indexing."""
+
+def Menu():
+
+  print("\n\n------------------------------"\
+  "\n     TO-DO LIST MANAGER"\
+  "\n------------------------------"\
+  '\n1. Add a Task'\
+  '\n2. View all Tasks'\
+  '\n3. Mark a task as Complete'\
+  '\n4. Remove a completed task'\
+  '\n0. Exit\n')
+      
+
+
+
+Menu()
