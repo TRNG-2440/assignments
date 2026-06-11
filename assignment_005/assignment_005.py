84167f4c58b096bad43879a2e89fd1cd65b6269d
William-Mahnke <william.mahnke@gmail.com>
2026-06-10 21:52:59 -0700

    contact class refactor assignment 5

--- assignment_005/assignment_005.py
+++ assignment_005/assignment_005.py
@@ -1,5 +1,12 @@
+# contact class
+class Contact:
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
 # contact list
-contacts = []
+contacts: list[Contact] = []
 
 # outer loop for contact book
 while True:
@@ -12,6 +19,7 @@ while True:
     print("3. Search for a contact")
     print("4. Delete a contact")
     print("5. Edit a contact")
+    print("6. View Contacts Alphabetically")
     print("0. Exit")
     print("════════════════════════════════════════")
     
@@ -22,27 +30,26 @@ while True:
             print("Goodbye!")
             break
 
-        case 1: # add contact
+        case 1: # add contact 
             print("")
-            
             while True:
-                name = input("Enter name: ")
-                # name check
+                name = input("Enter a name: ")
+                duplicate = False
                 for contact in contacts:
-                    if contact[0].lower() == name.lower():
-                        print(f"Contact with {name} already exists.")
-                        print("")
-                        continue
+                    if contact.name.lower() == name.lower():
+                        duplicate = True
+                    if duplicate:
+                        print(f"Contact with {name} already exists. Please enter a different name.")
+                    print("")
                 else:
                     break
             phone = input("Enter a phone number: ")
             email = input("Enter an email address: ")
-            contact = (name, phone, email)
-            contacts.append(contact)
+            contacts.append(Contact(name, phone, email))
             print(f"\nContact added: {name}")
             print("")
-
-        case 2: # view all contacts
+            
+        case 2: # view contacts
             print("")
             if not contacts:
                 print("No contacts found. Returning to main menu.")
@@ -51,7 +58,7 @@ while True:
                 print("All Contacts:")
                 print("────────────────────────────────────────")
                 for i, contact in enumerate(contacts, 1):
-                    print(f"{i}. Name: {contact[0]}\n   Phone: {contact[1]}\n   Email: {contact[2]}\n────────────────────────────────────────")
+                    print(f"{i}. Name: {contact.name}\n   Phone: {contact.phone}\n   Email: {contact.email}\n────────────────────────────────────────")
                 print("")
 
         case 3: # search for contact
@@ -62,11 +69,11 @@ while True:
             else:
                 name = input("Enter name to search for: ")
                 for contact in contacts:
-                    if contact[0].lower() == name.lower():
+                    if contact.name.lower() == name.lower():
                         print(f"────────────────────────────────────────")
-                        print(f"Name: {contact[0]}")
-                        print(f"Phone: {contact[1]}")
-                        print(f"Email: {contact[2]}")
+                        print(f"Name: {contact.name}")
+                        print(f"Phone: {contact.phone}")
+                        print(f"Email: {contact.email}")
                         print(f"────────────────────────────────────────")
                         print("")
                         break
@@ -83,14 +90,14 @@ while True:
                 print("All Contacts:")
                 print("────────────────────────────────────────")
                 for i, contact in enumerate(contacts, 1):
-                    print(f"{i}. Name: {contact[0]}\n   Phone: {contact[1]}\n   Email: {contact[2]}\n────────────────────────────────────────")
+                    print(f"{i}. Name: {contact.name}\n   Phone: {contact.phone}\n   Email: {contact.email}\n────────────────────────────────────────")
                 print("")
 
                 while True: # repeat until valid number entered
                     number = int(input("Enter the number of the contact to delete: "))
                     if 1 <= number <= len(contacts):
                         deleted_contact = contacts.pop(number - 1)
-                        print(f"\nContact deleted: {deleted_contact[0]}")
+                        print(f"\nContact deleted: {deleted_contact.name}")
                         print("")
                         break
                     else:
@@ -106,19 +113,19 @@ while True:
                 print("All Contacts:")
                 print("────────────────────────────────────────")
                 for i, contact in enumerate(contacts, 1):
-                    print(f"{i}. Name: {contact[0]}\n   Phone: {contact[1]}\n   Email: {contact[2]}\n────────────────────────────────────────")
+                    print(f"{i}. Name: {contact.name}\n   Phone: {contact.phone}\n   Email: {contact.email}\n────────────────────────────────────────")
                 print("")
             
             while True: # repeat until valid number entered
                 number = int(input("Enter the number of the contact to edit: "))
                 if 1 <= number <= len(contacts):
-                    print(f"Contact to edit: {contacts[number - 1][0]}")
+                    print(f"Contact to edit: {contacts[number - 1].name}")
                     contacts.pop(number - 1)
                     print("")
                     name = input("Enter new name: ")
                     phone = input("Enter new phone number: ")
                     email = input("Enter new email address: ")
-                    contact = (name, phone, email)
+                    contact = Contact(name, phone, email)
                     contacts.insert(number - 1, contact)
 
                     print(f"\nContact edited: {name}")
@@ -127,4 +134,17 @@ while True:
                 else:
                     print(f"Invalid number: {number}. Please try again.")
                     print("")
+
+        case 6:
+            print("")
+            if not contacts:
+                print("No contacts found. Returning to main menu")
+                print("")
+            else:
+                print("All Contacts:")
+                print("────────────────────────────────────────")
+                sorted_contacts = sorted(contacts, key=lambda x: x.name)
+                for i, contact in enumerate(sorted_contacts, 1):
+                    print(f"{i}. Name: {contact.name}\n   Phone: {contact.phone}\n   Email: {contact.email}\n────────────────────────────────────────")
+                print("")
             
