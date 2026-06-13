ef0cba4f0a39ac487c92d5917648e7281d1505ff
Ariyan Shaikh <ariyanattt@gmail.com>
2026-06-13 10:48:33 -0700

    Assignment 7 progress upload

--- assignment_007/assignment_007.py
+++ assignment_007/assignment_007.py
@@ -1,8 +1,35 @@
 # Assignment 7 by Ariyan Shaikh
 
 class Product:
-    def __init__(self):
-        pass
+    """
+    Product base class used to implement.
+    Phisical product, Digital produce and perishiable product
+    This is an abstract class.
+    """
+    def __init__(self, product_id: str, name: str, price: float, stock_quantity: int):
+        self.product_id = product_id
+        self.name = name
+        self.price = price
+        self.stock_quantity = stock_quantity
+    
+    def display_product_details(self) -> None:
+        """
+        Prints product all product attributes
+        """
+        print(f"Product ID: {self.product_id}")
+        print(f"Name: {self.name}")
+        print(f"Price: ${self.price:.2f}")
+        print(f"Stock quantity: {self.stock_quantity}")
+    
+    def get_price(self, quantity: int) -> float:
+        """
+        This function takes a quantity as an int parm and returns the total price for that amount
+        """
+        return round(self.price * quantity, 2)
+
+class PhysicalProduct(Product):
+    def __init__(self, product_id, name, price, stock_quantity):
+        super().__init__(product_id, name, price, stock_quantity)
 
 def get_selection(num_selections: int) -> int:
     """
