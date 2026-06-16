2f61ec2748843b0191d51d27e7744ec98780f4ad
Ariyan Shaikh <ariyanattt@gmail.com>
2026-06-15 22:32:40 -0700

    Partially completed assignment 9

---
+++ assignment_009/test_shopping_cart.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from cart import ShoppingCart
+from cart_exceptions import CartError
+
+class TestShoppingCart(unittest.TestCase):
+    def setUp(self):
+        self.shopping_cart = ShoppingCart()
+    
+    def tearDown(self):
+        self.shopping_cart.clear()
+
+    def test_item_management(self):
+        """
+        Tests adding, removing and updating quantity of items in the shopping cart class.
+        """
+        # Local variables
+        shopping_cart = self.shopping_cart
+        
+        # Test add_item function
+        shopping_cart.add_item("SKU-004", 1)
+        self.assertEqual(shopping_cart.get_items()[0]["sku"], "SKU-004")
+
+        # Test pdate quantity function
+        shopping_cart.update_quantity("SKU-004", 10)
+        self.assertEqual(shopping_cart.get_item_count(), 10)
+
+        # Test remove item function
+        shopping_cart.remove_item("SKU-004")
+        self.assertTrue(shopping_cart.is_empty)
+
+    
+if __name__ == "__main__":
+    unittest.main()
+
