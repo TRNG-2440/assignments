3668142906c61cc663b54c455444771865103812
Luke Fields <luke.fields@protonmail.com>
2026-06-25 14:29:16 -0700

    assignment 9 complete

---
+++ sql_assignment_009/assignment_009.sql
@@ -0,0 +1,18 @@
+SELECT d.doctor_name FROM doctor d
+WHERE d.doctor_id NOT IN (
+	SELECT a.doctor_id
+	FROM appointment a
+	JOIN doctor d ON a.doctor_id = d.doctor_id
+	GROUP BY a.doctor_id
+	HAVING COUNT(a.doctor_id) > 0
+);
+
+SELECT d.doctor_name FROM doctor d
+WHERE d.doctor_id NOT IN (
+	SELECT a.doctor_id
+	FROM appointment a
+	JOIN doctor d ON a.doctor_id = d.doctor_id
+    WHERE LOWER(a.status) != LOWER('Completed')
+	GROUP BY a.doctor_id
+	HAVING COUNT(a.doctor_id) > 0
+);
