2aa498607e5f53bf07cfef8a2f29574555cf0043
Luke Fields <luke.fields@protonmail.com>
2026-06-24 18:08:34 -0700

    oopsies

---
+++ sql_assignment_002/assignment_002.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA IF NOT EXISTS assignment_002;
+
+DROP TABLE IF EXISTS assignment_002.principle CASCADE;
+DROP TABLE IF EXISTS assignment_002.school CASCADE;
+DROP TABLE IF EXISTS assignment_002.student CASCADE;
+
+CREATE TABLE assignment_002.principle (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(32),
+    last_name VARCHAR(32),
+    salary INT
+);
+
+CREATE TABLE assignment_002.school (
+    id SERIAL PRIMARY KEY,
+    principle_id INT NOT NULL,
+    school_name VARCHAR(64),
+    CONSTRAINT principle_id_fk FOREIGN KEY (principle_id) REFERENCES assignment_002.principle(id)
+);
+
+CREATE TABLE assignment_002.student (
+    id SERIAL PRIMARY KEY,
+    school_id INT,
+    grade VARCHAR(4),
+    address VARCHAR(64),
+    emergency_phone CHAR(10),
+    CONSTRAINT school_id_fk FOREIGN KEY (school_id) REFERENCES assignment_002.school(id)
+);
