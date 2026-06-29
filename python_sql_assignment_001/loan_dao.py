fbdf0cfd245130970cde912c772c12257027ce2a
Suelem Audelo <84360844+saudelo@users.noreply.github.com>
2026-06-28 19:33:30 -0700

    Finished last two DAO

---
+++ python_sql_assignment_001/loan_dao.py
@@ -0,0 +1,108 @@
+from psycopg.rows import dict_row
+from dataclasses import dataclass
+from typing import Optional
+from datetime import date
+
+#dataclasses
+@dataclass
+class LoanRecord:
+    loan_id: int
+    book_id: int
+    member_id: int
+    loan_date: date
+    due_date: date
+    return_date: Optional[date]
+   
+## =============================================================================
+# DAO CLASS
+# =============================================================================
+class LoanDAO:
+    def __init__(self,conn):
+        # Connection string is built once at instantiation from environment
+        # variables loaded by db_util in main. All methods reuse this value.
+        self._conn = conn
+
+
+    def _map_row(self, row) -> LoanRecord:
+            # Private helper that converts a psycopg Row (dict-style) into an
+            # dataclass instance.
+
+            return LoanRecord(
+                loan_id=row["loan_id"],
+                book_id=row["book_id"],
+                member_id = row["member_id"],
+                loan_date=row["loan_date"],
+                due_date=row["due_date"],
+                return_date = row["return_date"]
+            )
+    
+    def get_all(self) -> list[LoanRecord]:
+        # Retrieves every row from the Book table.
+        # Returns an empty list if no records exist.
+        with self._conn.transaction():
+            with self._conn.cursor(row_factory=dict_row) as cursor:
+                cursor.execute(
+                    "SELECT loan_id, book_id, member_id, loan_date, due_date, return_date FROM books.loan"
+                )
+                return [self._map_row(row) for row in cursor.fetchall()]
+            
+    def create(self, book_id:int, member_id:int, loan_date:date, due_date:date, return_date:date) -> Optional[LoanRecord]:
+            with self._conn.cursor(row_factory=dict_row) as cursor:
+                cursor.execute(
+                    "INSERT INTO books.loan(book_id, member_id, loan_date, due_date,return_date) VALUES (%s,%s,%s,%s,%s) RETURNING *",
+                    (book_id,member_id,loan_date,due_date,return_date)
+                )
+                row = cursor.fetchone()
+                return self._map_row(row) if row else None
+    
+    def get_by_id(self, loan_id:int)-> Optional[LoanRecord]:  #return a single loan by its ID
+        # Retrieves a single loan by primary key.
+        # Returns None if no record with the given loan_id exists —
+        with self._conn.transaction():
+            with self._conn.cursor(row_factory=dict_row) as cursor:
+                cursor.execute(
+                    "SELECT book_id, member_id, loan_date, due_date,return_date FROM books.loan WHERE loan_id = %s",
+                    (loan_id,)
+                )
+                row = cursor.fetchone()
+                return self._map_row(row) if row else None
+    
+    def update(self, loan_id:int,book_id:int, member_id:int, loan_date:date, due_date:date, return_date:date) -> Optional[LoanRecord]:
+        # Performs a full replacement of an existing laon record.
+        # All fields are overwritten with the values provided in the record.
+        # RETURNING retrieves the updated row directly — if nothing is returned,
+        # no record with the given loan_id existed and None is returned to the caller.
+        with self._conn.transaction():
+            with self._conn.cursor(row_factory=dict_row) as cursor:
+                cursor.execute(
+                    """
+                    UPDATE books.loan
+                    SET title = %s, author = %s, pub_year = %s, genre_id =%s,copies = %s
+                    WHERE loan_id = %s
+                    RETURNING *
+                    """,
+                    (book_id, member_id, loan_date, due_date,return_date,loan_id)
+                )
+                row = cursor.fetchone()
+                return self._map_row(row) if row else None
+            
+    def delete(self, loan_id: int) -> bool:
+        # Deletes a loan record by primary key.
+        # RETURNING loan_id is used to detect whether a row was actually deleted —
+        # if fetchone() returns None, no record with the given loan_id existed.
+        # Returns True if a record was deleted, False if no match was found.
+        with self._conn.transaction():
+            with self._conn.cursor(row_factory=dict_row) as cursor:
+                cursor.execute(
+                    "DELETE FROM books.loan WHERE loan_id = %s RETURNING loan_id",
+                    (loan_id,)
+                )
+                return cursor.fetchone() is not None
+         
+
+
+
+                
+            
+    
+    
--- python_sql_assignment_001/member_dao.py
+++ python_sql_assignment_001/member_dao.py
@@ -1,93 +1,98 @@
 from psycopg.rows import dict_row
 from dataclasses import dataclass
 from typing import Optional
-
+from datetime import date
 #dataclasses
 @dataclass
-class GenreRecord:
-    genre_id: int
-    genre_name: str
+class MemberRecord:
+    member_id: int
+    full_name: str
+    email: str
+    date_joined: date
+    
    
 ## =============================================================================
 # DAO CLASS
 # =============================================================================
-class GenreDAO:
+class MemberDAO:
     def __init__(self,conn):
         # Connection string is built once at instantiation from environment
-        # variables loaded by db_util. All methods reuse this value.
-        self.conn = conn
+        # variables loaded by db_util in main. All methods reuse this value.
+        self._conn = conn
 
 
-    def _map_row(self, row) -> GenreRecord:
+    def _map_row(self, row) -> MemberRecord:
             # Private helper that converts a psycopg Row (dict-style) into an
             # dataclass instance.
 
-            return GenreRecord(
-                genre_id=row["genre_id"],
-                genre_name=row["genre_name"] 
+            return MemberRecord(
+                member_id=row["member_id"],
+                full_name=row["full_name"],
+                email = row["email"],
+                date_joined=row["date_joined"]
             )
     
-    def get_all(self) -> list[GenreRecord]:
-        # Retrieves every row from the genre table.
+    def get_all(self) -> list[MemberRecord]:
+        # Retrieves every row from the Member table.
         # Returns an empty list if no records exist.
         with self._conn.transaction():
             with self._conn.cursor(row_factory=dict_row) as cursor:
                 cursor.execute(
-                    "SELECT genre_id, genre_name FROM books.genre"
+                    "SELECT member_id, full_name, email, date_joined FROM books.member"
                 )
                 return [self._map_row(row) for row in cursor.fetchall()]
             
-    def create(self, genre_name:str) -> Optional[GenreRecord]:
+    def create(self, full_name:str, email:str,date_joined:date) -> Optional[MemberRecord]:
             with self._conn.cursor(row_factory=dict_row) as cursor:
                 cursor.execute(
-                    "INSERT INTO books.genre(genre_name) VALUES (%s) RETURNING *",
-                    (genre_name)
+                    "INSERT INTO books.member(full_name,email,date_joined) VALUES (%s,%s,%s) RETURNING *",
+                    (full_name,email,date_joined)
                 )
                 row = cursor.fetchone()
                 return self._map_row(row) if row else None
     
-    def get_by_id(self, genre_id:int)-> Optional[GenreRecord]:  #return a single book by its ID
-        # Retrieves a single employee by primary key.
-        # Returns None if no record with the given emp_id exists —
+    def get_by_id(self, member_id:int)-> Optional[MemberRecord]:  #return a single member by its ID
+        # Retrieves a single member by primary key.
+        # Returns None if no record with the given member_id exists —
         # the caller (endpoint handler) is responsible for raising a 404.
         with self._conn.transaction():
             with self._conn.cursor(row_factory=dict_row) as cursor:
                 cursor.execute(
-                    "SELECT genre_id, genre_name FROM books.genre WHERE genre_id = %s",
-                    (genre_id,)
+                    "SELECT member_id, full_name, email, date_joined FROM books.member WHERE member_id = %s",
+                    (member_id,)
                 )
                 row = cursor.fetchone()
                 return self._map_row(row) if row else None
     
-    def update(self, genre_id:int, genre_name:str) -> Optional[GenreRecord]:
-        # Performs a full replacement of an existing genre record.
+    def update(self, member_id:int,full_name:str, email:str, date_joined) -> Optional[MemberRecord]:
+        # Performs a full replacement of an existing member record.
         # All fields are overwritten with the values provided in the record.
         # RETURNING retrieves the updated row directly — if nothing is returned,
-        # no record with the given genre_id existed and None is returned to the caller.
+        # no record with the given member_id existed and None is returned to the caller.
         with self._conn.transaction():
             with self._conn.cursor(row_factory=dict_row) as cursor:
                 cursor.execute(
                     """
-                    UPDATE books.genre
-                    SET genre_name = %s
-                    WHERE genre_id = %s
+                    UPDATE books.member
+                    SET full_name = %s, email = %s, date_joined = %s
+                    WHERE member_id = %s
                     RETURNING *
                     """,
-                    (genre_name,genre_id)
+                    (full_name, email, date_joined,member_id)
                 )
                 row = cursor.fetchone()
                 return self._map_row(row) if row else None
             
-    def delete(self, genre_id: int) -> bool:
-        # Deletes a genre record by primary key.
-        # RETURNING genre_id is used to detect whether a row was actually deleted —
+    def delete(self, member_id: int) -> bool:
+        # Deletes a member record by primary key.
+        # RETURNING member_id is used to detect whether a row was actually deleted —
         # if fetchone() returns None, no record with the given book_id existed.
         # Returns True if a record was deleted, False if no match was found.
         with self._conn.transaction():
             with self._conn.cursor(row_factory=dict_row) as cursor:
                 cursor.execute(
-                    "DELETE FROM books.genre WHERE genre_id = %s RETURNING genre_id",
-                    (genre_id,)
+                    "DELETE FROM books.member WHERE member_id = %s RETURNING member_id",
+                    (member_id,)
                 )
                 return cursor.fetchone() is not None
          
